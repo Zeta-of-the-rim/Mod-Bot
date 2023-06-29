e41e7776613d8026a4d5b582474246fe284c26dc
Terenzo Burgess <tburgess@concordia.sa.edu.au>
2023-06-29 11:35:43 +0930

    First commit. Don't know if this will be finished or not.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+using Newtonsoft.Json;
+using static Newtonsoft.Json.Formatting;
+
+namespace ModBot
+{
+    class StartUp
+    {
+        public Boolean Debug = false;
+        
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Thank you for choosing ModBot!");
+            if (File.Exists("DEBUG"))
+            {
+                // This is not a good way to do this, but it should allow users to debug the bot without tools
+                Console.WriteLine("Running in debug. This is not recommended for regular use. \n Remove the file DEBUG to disable debug mode.");
+            }
+            // Check if a token file exists
+            if (!File.Exists("token.txt"))
+            {
+                // If not, create one
+                File.Create("token.txt");
+                // Throw an error
+                Console.WriteLine("No token file found. Please enter your token in token.txt");
+                Environment.Exit(400);
+            }
+            // Read the token file
+            string Token = File.ReadAllText("token.txt");
+            
+            // Now we read the config file (this ships with the bot)
+            string Config = File.ReadAllText("config.json");
+            // Deserialize the config file (I have no idea what im doing)
+            
+
+
+
+
+
+
+            var Discord_client = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = Token,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged
+            });
+            // Register commands
+            Discord_client.UseCommandsNext(new CommandsNextConfiguration()
+            {
+                StringPrefixes = new string[] { "!" }
+            });
+            
+            Thread.Sleep(-1);
+            // Holding the program open
+        }
+    }        
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+﻿# Mod-Bot
+Mod-Bot is a Simple Discord Bot that utilizes OpenAI's Moderation Endpoint to pefrom moderation tasks on Discord Servers.
+It allows users to tag what they deem as assertable and automatically removes it from the server.
+
+## Limitations
+
+I AM NOT and Never will be paying for other people's API usage. Is means that if you will need
+
+- Your own Discord Application and Bot Token
+- Your own OpenAI API Key
+- Your own hosting solution
+
+## Installation
+
+1. Clone the repository
+2. Install the requirements
+3. Open the `Tokens.json` file and add your Discord Bot Token and OpenAI API Key
+4. Open the `config.json` file and just the settings to your liking (this may require some trial and error and I recommend getting your server to help you)
+5. Run the bot
